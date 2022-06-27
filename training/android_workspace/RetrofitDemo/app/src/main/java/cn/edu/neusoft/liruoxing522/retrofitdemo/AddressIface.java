53c8d6ab8545933df3101d1f466adcff02730d96
x-rush <779581099@qq.com>
2022-06-28 00:46:40 +0800

    android trainning update

--- training/android_workspace/RetrofitDemo/app/src/main/java/cn/edu/neusoft/liruoxing522/retrofitdemo/AddressIface.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.neusoft.liruoxing522.retrofitdemo;
-
-/**
- * Created by Administrator on 2017/10/11.
- */
-
-public interface AddressIface {
-    void getAddressList(String address,AddressListener addressListener);
-}
